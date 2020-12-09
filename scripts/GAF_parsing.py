3872c37c61d26e40dc29b8776dd452eaa40ed893
mrvollger <mrvollger@gmail.com>
2020-12-08 20:45:49 -0800

    lots of fixes / dev

--- scripts/GAF_parsing.py
+++ scripts/GAF_parsing.py
@@ -4,15 +4,27 @@ import os
 import sys
 import pandas as pd 
 import re
-
+global args
 first = True
 
 def describe(row):
-    f1 = "fc"
-    f2 = "fc"
-    if row.strand == "-" : f1 = "rc"
-    if row.direction == "<": f2 = "rc"
-    return f"the {f1} of {row.q}:{row.qs}-{row.qe} aligns to the {f2} of {row.r}:{row.rs}-{row.re}"
+    size = "{:.0f}kbp".format( (row.qe - row.qs)/1000.0 )
+    sm = row.r
+    if (row.strand == "+" and row.direction == ">") or (row.strand == "-" and row.direction == "<"):
+        t = "INS"
+    else:
+        t = "INV"
+    if(sm == args.ref and t == "INS"):
+        return(f"Syntenic") 
+    else:
+        return("{}_{}".format(t, size, sm))
+
+    #f1 = "fc"
+    #f2 = "fc"
+    #if row.strand == "-" : f1 = "rc"
+    #if row.direction == "<": f2 = "rc"
+    #return f"the {f1} of {row.q}:{row.qs}-{row.qe} aligns to the {f2} of {row.r}:{row.rs}-{row.re}"
+    
 
 
 
@@ -31,8 +43,13 @@ def parse_path(row):
     #print()
     #print(paths) 
     # path through the graph
-    for path in paths:
+    first = True
+    for idx, path in enumerate(paths):
         direction, ref, start, end = path[0], path[1], int(path[2]),int(path[3])
+        if(idx==0):
+            start = start + row.paths
+        if(idx == len(paths)-1 ):
+            end = end - (row.pathl - row.pathe)
 
         qs = row.qs
         qe = qs + end-start
@@ -46,8 +63,10 @@ def parse_path(row):
 
     out = pd.DataFrame(out, columns=["q", "qs", "qe", "ql", "r", "rs", "re", "strand", "direction"])
     out["description"] = out.apply(describe, axis=1)
-    out.sort_values(by=["q","qs"]).to_csv(sys.stdout, sep="\t", header=first, index=False)
+    #out.sort_values(by=["q","qs"]).to_csv(sys.stdout, sep="\t", header=first, index=False)
+    out.sort_values(by=["q","qs"], inplace=True)
     first=False
+    return(out)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -64,4 +83,26 @@ if __name__ == "__main__":
     gaf["reference"] = args.ref
     #print(gaf[colnames[0:7]])
 
-    gaf.apply(parse_path, axis = 1)
+    out = pd.concat( list(gaf.apply(parse_path, axis = 1)) )
+    out.sort_values(by=["q","qs"]).to_csv(sys.stdout, sep="\t", header=True, index=False)
+    for (q, r), group in out.groupby(by = ["q", "r"]):
+        syn = group[group.description == "Syntenic"]
+        if(r != args.ref or syn.shape[0] <= 1):
+            continue
+        shift = syn.shift(periods=1)
+        deletions = ((syn.qs - shift.qe).abs() < 5000)
+        for idx, d in enumerate(deletions):
+            if(not d):
+                continue
+            cur = syn.iloc[idx -1]
+            nxt = syn.iloc[idx]
+            size = "{:.0f}kbp".format( (nxt.rs - cur.re)/1000 )
+            #print(cur, nxt, size)
+            print(f"{cur.q}\t{cur.qe}\t{cur.qe}\t{cur.ql}\t{cur.r}\t{cur.re}\t{nxt.rs}\t+\t>\tDEL_{size}")
+    
+
+
+
+
+
+
